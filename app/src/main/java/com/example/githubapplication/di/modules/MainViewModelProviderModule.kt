cbbf1c4515d387073a086fad84f329abe0768d06
Jitendra Reddy Syamala <jithendra194reddy@gmail.com>
2020-12-07 18:38:43 -0500

    Created ViewModel Providers Dagger implementation

--- app/src/main/java/com/example/githubapplication/di/modules/MainViewModelProviderModule.kt
+++ app/src/main/java/com/example/githubapplication/di/modules/MainViewModelProviderModule.kt
@@ -1,8 +1,16 @@
 package com.example.githubapplication.di.modules
 
+import androidx.lifecycle.ViewModel
+import com.example.githubapplication.MainViewModel
+import com.example.githubapplication.di.viewmodel_providers.ViewModelKey
+import dagger.Binds
 import dagger.Module
+import dagger.multibindings.IntoMap
 
 @Module
 abstract class MainViewModelProviderModule {
-
+    @Binds
+    @IntoMap
+    @ViewModelKey(MainViewModel::class)
+    abstract fun providesMainViewModel(viewModel: MainViewModel): ViewModel
 }
---
+++ app/src/main/java/com/example/githubapplication/di/viewmodel_providers/ViewModelFactoryModule.kt
@@ -0,0 +1,11 @@
+package com.example.githubapplication.di.viewmodel_providers
+
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class ViewModelFactoryModule {
+    @Binds
+    abstract fun bindViewModelFactory(viewModelProviderFactory: ViewModelProviderFactory): ViewModelProvider.Factory
+}
